6cf1be8018779790aaaf3feb663ad67b4f843492
Vikas Dhiman <wecacuee@users.noreply.github.com>
2020-04-22 13:02:59 -0700

    cleaner way to do things

--- darknet_ros/src/YoloObjectDetector.cpp
+++ darknet_ros/src/YoloObjectDetector.cpp
@@ -577,8 +577,7 @@ IplImage* newIplImage(T& m) {
   // https://github.com/opencv/opencv/commit/ad146e5a6b931ac5d179d4fedc58cef99a3c9e7e
   CV_Assert( m.dims <= 2);
   IplImage* self = new IplImage();
-  cvInitImageHeader(self, cvSize(m.rows, m.cols), cvIplDepth(m.flags), m.channels());
-  cvSetData(self, m.data, (int)m.step[0]);
+  *self = cvIplImage(m);
   return self;
 }
 
