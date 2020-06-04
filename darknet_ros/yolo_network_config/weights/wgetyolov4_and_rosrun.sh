8651cf6ff70e4ebfded5265b7752d45c86096d10
Vikas Dhiman <wecacuee@users.noreply.github.com>
2020-06-04 14:16:14 -0700

    BASH_SOURCE not BASHSOURCE

--- darknet_ros/yolo_network_config/weights/wgetyolov4_and_rosrun.sh
+++ darknet_ros/yolo_network_config/weights/wgetyolov4_and_rosrun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DIR=$(cd $(dirname ${BASHSOURCE[0]}) && pwd)
+DIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 if [ ! -f $DIR/yolov4.weights ];then 
     echo "Downloading Yolov4 weights"
     wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights -O $DIR/yolov4.weights
